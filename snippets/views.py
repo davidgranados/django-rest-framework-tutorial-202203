04a32347f340f5aa5003cc842c5fb8a78329141a
David <davidgranados73@gmail.com>
2022-03-16 21:19:38 -0500

    viewset binded explicitly

--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.http import Http404, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics, mixins, permissions, status, renderers
-from rest_framework.decorators import api_view
+from rest_framework import generics, mixins, permissions, renderers, status, viewsets
+from rest_framework.decorators import action, api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
@@ -100,6 +100,27 @@ class SnippetList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         return serializer.save(owner=self.request.user)
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides
+    `list`, `create`, `retrieve`, `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 @csrf_exempt
@@ -216,6 +237,15 @@ class UserList(generics.ListAPIView):
 
 
 class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `retrieve` actions.
+    """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
